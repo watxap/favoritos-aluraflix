42a00d284269a28c885797d82a304cda06818656
Matias Paz <neoxap@gmail.com>
2023-11-10 22:00:44 -0300

    Borrado elemento

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,39 @@
-function agregarPelicula(){
+var eliminar = document.getElementsByClassName("form-wrapper");
+for (let i = 0; i < eliminar.length; i++) {
+  eliminar[i].parentNode.removeChild(eliminar[i]);
+}
+ 
+
+document.write(`<div class="form-wrapper">
+<input type="text" id="pelicula" name="pelicula" placeholder="Dirección URL del poster">
+<input type="text" id="trailer" name="trailer" placeholder="Link del trailer en YouTube">
+<button onClick="agregarTrailer()">Agregar trailer</button>
+</div>`)
+
+//rehago el div entero para que quede mejor organizado estéticamente, sin retocar el documento .html 
+
+
+
+/*
+
+document.write(`<div class="form-wrapper">
+<input type="text" id="trailer" name="trailer" placeholder="Link del trailer en YouTube">
+<button onClick="agregarTrailer()">Agregar trailer</button>
+</div>`)
+
+*/
+
+/*function agregarPelicula(){
     var peliculaFavorita = document.getElementById('pelicula').value
     var elementoListaPeliculas = document.getElementById('listaPeliculas')
     elementoListaPeliculas.innerHTML = elementoListaPeliculas.innerHTML + '<img src=' + peliculaFavorita + '>'
     document.getElementById('pelicula').value = ''
-    console.log(elementoListaPeliculas)
-  }
+    }
+
+function agregarTrailer(){
+      var peliculaFavorita = document.getElementById('pelicula').value
+      var elementoListaPeliculas = document.getElementById('listaPeliculas')
+      elementoListaPeliculas.innerHTML = elementoListaPeliculas.innerHTML + '<img src=' + peliculaFavorita + '>'
+      document.getElementById('pelicula').value = ''
+      }
+  */
