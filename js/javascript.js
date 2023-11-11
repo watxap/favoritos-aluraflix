2c83178b09d0912d9c780ff4317419d1707d81af
Matias Paz <neoxap@gmail.com>
2023-11-11 03:15:13 -0300

    Completado desafío 2

--- js/javascript.js
+++ js/javascript.js
@@ -2,38 +2,20 @@ var eliminar = document.getElementsByClassName("form-wrapper");
 for (let i = 0; i < eliminar.length; i++) {
   eliminar[i].parentNode.removeChild(eliminar[i]);
 }
- 
 
 document.write(`<div class="form-wrapper">
 <input type="text" id="pelicula" name="pelicula" placeholder="Dirección URL del poster">
 <input type="text" id="trailer" name="trailer" placeholder="Link del trailer en YouTube">
-<button onClick="agregarTrailer()">Agregar trailer</button>
+<button onClick="agregarPelicula()">Agregar película</button>
 </div>`)
 
-//rehago el div entero para que quede mejor organizado estéticamente, sin retocar el documento .html 
+//se rehace el div entero para que quede mejor organizado estéticamente, sin retocar el documento .html 
 
-
-
-/*
-
-document.write(`<div class="form-wrapper">
-<input type="text" id="trailer" name="trailer" placeholder="Link del trailer en YouTube">
-<button onClick="agregarTrailer()">Agregar trailer</button>
-</div>`)
-
-*/
-
-/*function agregarPelicula(){
-    var peliculaFavorita = document.getElementById('pelicula').value
-    var elementoListaPeliculas = document.getElementById('listaPeliculas')
-    elementoListaPeliculas.innerHTML = elementoListaPeliculas.innerHTML + '<img src=' + peliculaFavorita + '>'
-    document.getElementById('pelicula').value = ''
-    }
-
-function agregarTrailer(){
+function agregarPelicula(){
       var peliculaFavorita = document.getElementById('pelicula').value
+      var trailerPelicula = document.getElementById('trailer').value
       var elementoListaPeliculas = document.getElementById('listaPeliculas')
-      elementoListaPeliculas.innerHTML = elementoListaPeliculas.innerHTML + '<img src=' + peliculaFavorita + '>'
+      elementoListaPeliculas.innerHTML = elementoListaPeliculas.innerHTML + `<a href=${trailerPelicula} target="_blank"><img src="${peliculaFavorita}"></a>`
       document.getElementById('pelicula').value = ''
+      document.getElementById('trailer').value = ''
       }
-  */
